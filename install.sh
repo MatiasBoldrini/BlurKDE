d709aa3c47ba73ab2abc4cf533eb88cc2d9be3de
ShadowFighter99 <50892896+ShadowFighter99@users.noreply.github.com>
2020-03-11 16:35:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,8 @@ sudo apt -y install software-properties-common
    
  fi
  #instalar Wallpaper y fuentes
- 
+ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
  #instalar Akava tema
  git clone https://github.com/Akava-Design/Akava-Colors
  cd Akava-Colors
