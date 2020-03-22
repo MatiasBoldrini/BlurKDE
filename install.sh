662665d3ab5e54b97815d51bd70982a8580e0356
Matías <50892896+ShadowFighter99@users.noreply.github.com>
2020-03-22 17:42:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sleep 1
 echo "El instalador esta hecho especificamente para arch , ubuntu y derivados"
 echo "Si estas usando Fedora u otra distro , Puedes Cancelar el instalador ahora , o intentar instalarlo (Solo para Fedora)"
   sleep 5
-  YUM_CMD=$(which yum)
+  YUM_CMD=$(which dnf)
   APT_GET_CMD=$(which apt-get)
   PACMAN_CMD=$(which pacman)
  if [[ ! -z $YUM_CMD ]]; then
@@ -12,15 +12,21 @@ echo "Si estas usando Fedora u otra distro , Puedes Cancelar el instalador ahora
     sudo dnf install t5-style-kvantum qt5-style-kvantum-themes latte-dock zsh curl
     sudo dnf install git g++ extra-cmake-modules cmake gettext libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libfftw3-dev
     sudo dnf install kdecoration qt5-declarative qt5-x11extras kcoreaddons kguiaddons kconfigwidgets kwindowsystem fftw cmake extra-cmake-modules
+ sudo dnf install cmake extra-cmake-modules kf5-kconfig-devel
+   sudo dnf install   kf5-kcoreaddons-devel kf5-kwindowsystem-devel kwin-devel 
+   sudo dnf install   qt5-qtbase-devel
  elif [[ ! -z $APT_GET_CMD ]]; then
 sudo apt -y install software-properties-common
  	sudo add-apt-repository -y  ppa:papirus/papirus
-
+sudo apt install cmake extra-cmake-modules kwin-dev 
+   sudo apt install libkf5config-dev libkf5configwidgets-dev libkf5coreaddons-dev 
+  sudo apt install libkf5windowsystem-dev qtbase5-dev
     sudo apt -y install qt5-style-kvantum qt5-style-kvantum-themes latte-dock zsh curl 
     sudo apt -y install git g++ extra-cmake-modules cmake gettext libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libfftw3-dev
 
  elif [[ ! -z $PACMAN_CMD ]]; then
     sudo pacman -S kdecoration qt5-declarative qt5-x11extras kcoreaddons kguiaddons kconfigwidgets kwindowsystem fftw cmake extra-cmake-modules
+sudo pacman -S cmake extra-cmake-modules kwin
 
     sudo pacman -S qt5-style-kvantum qt5-style-kvantum-themes latte-dock zsh curl
  else
@@ -36,7 +42,14 @@ sudo apt -y install conky-themes
 sudo dpkg -i realpath_8.26-3ubuntu4_all.deb conky-manager-v2.4-amd64.deb
 sudo apt -f install
 ####INstalacion de conky
-
+git clone https://github.com/zzag/kwin-effects-yet-another-magic-lamp.git
+cd kwin-effects-yet-another-magic-lamp
+mkdir build && cd build
+cmake .. 
+    -DCMAKE_BUILD_TYPE=Release 
+    -DCMAKE_INSTALL_PREFIX=/usr
+make
+sudo make install
  git clone https://github.com/Akava-Design/Akava-Colors
  cd Akava-Colors
  cp Akava.colors ~/.local/share/color-schemes/ 
